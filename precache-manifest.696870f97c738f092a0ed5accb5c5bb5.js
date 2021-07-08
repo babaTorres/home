1383443693dbcccbf65f4f6a901dd3c23a9ea4e9
Brandon Torres <torresbm95@gmail.com>
2021-07-08 11:25:35 -0400

    Updates

---
+++ precache-manifest.696870f97c738f092a0ed5accb5c5bb5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0c9c478a5b13af6254658facd56739",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "61d74505dbbf8be16781",
+    "url": "/home/static/css/main.19ae4401.chunk.css"
+  },
+  {
+    "revision": "84f9e62c2eb6019665ed",
+    "url": "/home/static/js/2.55b6ca0b.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.55b6ca0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d74505dbbf8be16781",
+    "url": "/home/static/js/main.90f37193.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "2c3546d5ad42ffd98dd8bbf28d5217f6",
+    "url": "/home/static/media/Brandon Torres Resume.2c3546d5.pdf"
+  },
+  {
+    "revision": "558bfc4ff3eb2a325535221d3e3711f3",
+    "url": "/home/static/media/UMlogo.558bfc4f.png"
+  },
+  {
+    "revision": "f05be9e7ef9104ab4c2414906f5efeff",
+    "url": "/home/static/media/fsu logo.f05be9e7.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "dd6c8989f87100223aec08c2b20e1754",
+    "url": "/home/static/media/propic.dd6c8989.jpg"
+  }
+]);
